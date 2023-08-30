3d0bd96041519e522589b5a5f79303eb51f80547
Rinku <rinku@appstechy.com>
2023-08-30 12:06:57 +0530

    upadted

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
+  host: env('HOST', 'https://strapi-backend-kyn2.onrender.com'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
